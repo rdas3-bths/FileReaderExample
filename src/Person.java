926d5ee21fa05e8c2d91a4c07dc1a2e6159f4892
rdas3-bths <rdas3@schools.nyc.gov>
2022-05-09 14:28:16 -0400

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,58 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
+
+public class Person {
+    private String name;
+    private String hobby;
+
+    public Person() {
+        name = "";
+        hobby = "";
+    }
+
+    public Person(String name, String hobby) {
+        this.name = name;
+        this.hobby = hobby;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public String greet() {
+        if (name.equals("") && hobby.equals("")) {
+            return "Hello! Please introduce yourself!";
+        }
+        else {
+            return "Name: " + name + "\n" + "Hobby: " + hobby;
+        }
+    }
+
+    public void save() {
+        try {
+            File f = new File("src/person.data");
+            f.createNewFile();
+            FileWriter fw = new FileWriter("src/person.data");
+            fw.write(name + "\n");
+            fw.write(hobby);
+            fw.close();
+        }
+        catch (IOException e) {
+            System.out.println("Unable to create file");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/PersonRunner.java
@@ -0,0 +1,63 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class PersonRunner {
+    public static void main(String[] args) {
+        try {
+            File f = new File("src/person.data");
+            Scanner s = new Scanner(f);
+            int line = 1;
+            String name = "";
+            String hobby = "";
+            while (s.hasNextLine()) {
+                String data = s.nextLine();
+                if (line == 1) {
+                    name = data;
+                }
+                if (line == 2) {
+                    hobby = data;
+                }
+                line++;
+            }
+            s.close();
+            Person p =  new Person(name, hobby);
+            System.out.println(p.greet());
+            System.out.println("Would you like to update any information? Select an option");
+            System.out.println("1: Change my name");
+            System.out.println("2: Change my hobby");
+            System.out.println("3: Change both hobby and name");
+            System.out.println("4: Exit");
+            s = new Scanner(System.in);
+            System.out.print("Enter your option: ");
+            String option = s.nextLine();
+            if (option.equals("1") || option.equals("3")) {
+                String n = "";
+                System.out.print("Enter your new name: ");
+                n = s.nextLine();
+                p.setName(n);
+            }
+            if (option.equals("2") || option.equals("3")) {
+                String h = "";
+                System.out.print("Enter your new hobby: ");
+                h = s.nextLine();
+                p.setHobby(h);
+            }
+            p.save();
+            System.out.println("Data saved! Good bye!");
+
+        }
+        catch (FileNotFoundException e) {
+            Person p = new Person();
+            System.out.println(p.greet());
+            System.out.print("What is your name? ");
+            Scanner in = new Scanner(System.in);
+            String name = in.nextLine();
+            System.out.print("What is your hobby? ");
+            String hobby = in.nextLine();
+            p.setName(name);
+            p.setHobby(hobby);
+            p.save();
+        }
+    }
+}
